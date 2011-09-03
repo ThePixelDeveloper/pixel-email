3e789740b2468c2151c5d8fa1530bff33b456f21
Mathew Davies <thepixeldeveloper@googlemail.com>
2011-09-03 19:45:04 +0100

    Route is now /emails instead of /email

--- init.php
+++ init.php
@@ -1,18 +1,18 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-Route::set('email', 'email')
+Route::set('email', 'emails')
 	->defaults(array(
 		'controller' => 'email',
 		'action'     => 'list'
 	));
 
-Route::set('email-inbox', 'email/<email>', array('email' => '[^/]+'))
+Route::set('email-inbox', 'emails/<email>', array('email' => '[^/]+'))
 	->defaults(array(
 		'controller' => 'email',
 		'action'     => 'inbox'
 	));
 
-Route::set('email-read', 'email/<email>(/<id>(/<part>))', array('email' => '[^/]+', 'id' => '[0-9]++', 'part' => '[a-zA-Z]++'))
+Route::set('email-read', 'emails/<email>(/<id>(/<part>))', array('email' => '[^/]+', 'id' => '[0-9]++', 'part' => '[a-zA-Z]++'))
 	->defaults(array(
 		'controller' => 'email',
 		'action'     => 'read'
