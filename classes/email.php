b12cbfb3a41fc0f10ff2e77ad4b12c5de812ee75
Mathew Davies <thepixeldeveloper@googlemail.com>
2011-09-05 15:36:09 +0100

    Merged in the email module.

---
+++ classes/email.php
@@ -0,0 +1,88 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+/**
+ * Email module
+ *
+ * Ported from Kohana 2.2.3 Core to Kohana 3.0 module
+ * 
+ * Updated to use Swiftmailer 4.0.4
+ *
+ * @package    Core
+ * @author     Kohana Team
+ * @copyright  (c) 2007-2008 Kohana Team
+ * @license    http://kohanaphp.com/license.html
+ */
+class Email
+{
+	/**
+	 * Creates a SwiftMailer instance.
+	 *
+	 * @param   string  DSN connection string
+	 * @return  object  Swift object
+	 */
+	public static function get_transport($config = NULL)
+	{
+		if ( ! class_exists('Swift_Mailer', FALSE))
+		{
+			require Kohana::find_file('vendor', 'swiftmailer/lib/swift_required');
+		}
+
+		// Load default configuration
+		($config === NULL) and $config = Kohana::$config->load('email');
+		
+		if ('smtp' === $config['driver'])
+		{
+			/**
+			 * SMTP transport
+			 */
+			$port = empty($config['options']['port']) ? 25 : (int) $config['options']['port'];
+			
+			$transport = Swift_SmtpTransport::newInstance($config['options']['hostname'], $port);
+
+			if ( ! empty($config['options']['encryption']))
+			{
+				$transport->setEncryption($config['options']['encryption']);
+			}
+			
+			empty($config['options']['username']) or $transport->setUsername($config['options']['username']);
+			empty($config['options']['password']) or $transport->setPassword($config['options']['password']);
+			
+			$transport->setTimeout(empty($config['options']['timeout']) ? 5 : (int) $config['options']['timeout']);
+		}
+		elseif ('sendmail' === $config['driver'])
+		{
+			/**
+			 * Sendmail transport
+			 */
+			$transport = Swift_SendmailTransport::newInstance(empty($config['options']) ? "/usr/sbin/sendmail -bs" : $config['options']);
+		}
+		elseif('null' === $config['driver'])
+		{
+			/**
+			 * Null transport
+			 */
+			$transport = Swift_NullTransport::newInstance();
+		}
+		else
+		{
+			/**
+			 * Native transport (mail).
+			 */
+			$transport = Swift_MailTransport::newInstance($config['options']);
+		}
+		
+		if (isset($config['logging']) AND $config['logging'])
+		{
+			/**
+			 * Logs all outgoing email to the database. 
+			 */
+			$model = ORM::factory('email');
+			
+			$database_reporter = new Swift_Plugins_Pixel_Email_Logger_Database($model);
+			$reporter          = new Swift_Plugins_ReporterPlugin($database_reporter);
+				
+			$transport->registerPlugin($reporter);
+		}
+		
+		return $transport;
+	}
+} // End email
---
+++ config/email.php
@@ -0,0 +1,36 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+return array
+(
+	/**
+	 * SwiftMailer driver, used with the email module.
+	 *
+	 * Valid drivers are: native, sendmail, smtp, null
+	 */
+	'driver' => 'sendmail',
+	
+	/**
+	 * To use secure connections with SMTP, set "port" to 465 instead of 25.
+	 * To enable TLS, set "encryption" to "tls".
+	 * 
+	 * Note for SMTP, 'auth' key no longer exists as it did in 2.3.x helper
+	 * Simply specifying a username and password is enough for all normal auth methods
+	 * as they are autodeteccted in Swiftmailer 4
+	 * 
+	 * PopB4Smtp is not supported in this module as I had no way to test it but 
+	 * SwiftMailer 4 does have a PopBeforeSMTP plugin so it shouldn't be hard to implement
+	 * 
+	 * Encryption can be one of 'ssl' or 'tls' (both require non-default PHP extensions
+	 *
+	 * Driver options:
+	 * @param   null    native: no options
+	 * @param   string  sendmail: executable path, with -bs or equivalent attached
+	 * @param   array   smtp: hostname, (username), (password), (port), (encryption)
+	 * @param   null    null: no options
+	 */
+	'options' => NULL,
+		
+	/**
+	 * Do we want to log the emails to the database?
+	 */
+	'logging' => FALSE,
+);
